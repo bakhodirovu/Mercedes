791f485d2eeb4094c7934cf5eba1183e3d43b718
UMID <bahodirovumid1@gmail.com>
2022-06-18 19:28:33 +0500

    +modal

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ window.addEventListener('DOMContentLoaded',function (){
     const menuBtn = document.querySelector('.menu_btn'),
         navigation = document.querySelector('.navbar_nav'),
         navigationItem = document.querySelectorAll('.navbar_nav a'),
-        scrolBtn = document.querySelector('.scrol_top_btn');
+        scrolBtn = document.querySelector('.scrol_top_btn'),
+        btn = document.querySelector('#btn'),
+        modalBg = document.querySelector('.modal_bg'),
+        closeModal = document.querySelector('.close');
+        
 
     menuBtn.addEventListener('click', ()=>{
         menuBtn.classList.toggle('active')
@@ -20,6 +24,12 @@ window.addEventListener('DOMContentLoaded',function (){
     scrolBtn.addEventListener('click',function(){
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
+    });
+    btn.addEventListener('click',function(){
+        modalBg.style.display = 'flex';
+    });
+    closeModal.addEventListener('click',function(){
+        modalBg.style.display = 'none';
     });
 
 
